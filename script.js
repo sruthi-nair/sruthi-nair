026b347bf8d0ea603fcb14dad9ad1dbbfca49ae7
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2023-04-12 09:52:39 +0530

    Added Portfolio Code

---
+++ script.js
@@ -0,0 +1,9 @@
+// typing animation
+
+// var typed = new Typed(".typing",{
+//     strings:["Web Designer", "UI/UX Designer", "Graphic Designer"],
+//     typespeed:100,
+//     Backspeed:60,
+//     loop: true
+// })
+
